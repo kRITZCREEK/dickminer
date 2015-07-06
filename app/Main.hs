c5a0b637b0e4c851eaa594a3d5884b038c6960a5
kRITZCREEK <christoph.hegemann@gmx.net>
2015-07-06 16:40:51 -0700

    read and concatenate input files and parse Entries

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Dickmine
+import           Dickmine
+import           System.Environment
+import           System.IO
+
+readLogFile :: FilePath  -> IO [String]
+readLogFile path = fmap lines (readFile path)
+
+splitIntoEntries :: [String] -> [[String]]
+splitIntoEntries file =
+  reverse $ splitIntoEntries' [] $ filter (/= "") file
+  where splitIntoEntries' acc ls
+          | length ls >= 6 = splitIntoEntries' (take 6 ls : acc) (drop 6 ls)
+          | otherwise      = acc
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  logFiles <- mapM readLogFile args
+  let entries = concatMap splitIntoEntries logFiles
+  print (map parseLogEntry entries)
