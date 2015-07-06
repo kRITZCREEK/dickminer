e5dcac8e46dc927ff9ea9eadf6c4cf0077898eab
kRITZCREEK <christoph.hegemann@gmx.net>
2015-07-06 09:10:23 -0700

    adds parsers for log entries.

---
+++ src/Dickmine/Parse.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE PatternGuards #-}
+module Dickmine.Parse where
+
+import Dickmine.Types
+import Data.Time
+import Data.List
+import Data.Char
+import Text.Read
+import Control.Applicative
+import System.Locale (defaultTimeLocale)
+
+logEntry :: [String]
+logEntry =
+  [
+    "07/06/2015 08:46:41 am 37.201.171.211 /",
+    "Country: (Unknown Country?) (XX)",
+    "City: (Unknown City?)",
+    "Latitude:",
+    "Longitude:",
+    "IP: 37.201.171.211"
+  ]
+
+parseLogEntry :: [String] -> Maybe Pagehit
+parseLogEntry [dateIPRoute, country, city, lat, long, ip] =
+  Pagehit <$> Just ip' <*> Just page' <*> lat' <*> long' <*> time'
+  where
+    (date, ip', page') = splitDateIPRoute dateIPRoute
+    country' = parseCountry country
+    city' = parseCity city
+    lat' = parseLatitude lat
+    long' = parseLongitude long
+    time' = parseDateString date
+parseLogEntry _ = Nothing
+
+splitDateIPRoute :: String -> (String, String, String)
+splitDateIPRoute s = (dateString, ipString, route)
+  where splitted = words s
+        dateString = unwords (take 3 splitted)
+        (ipString, route) = if length splitted <= 4
+                   then ("", "")
+                   else (splitted !! 3, splitted !! 4)
+
+
+parseDateString :: String -> Maybe UTCTime
+parseDateString = parseTime defaultTimeLocale "%m/%d/%Y %I:%M:%S %p"
+
+parseIP :: String -> Maybe String
+parseIP s =
+  if all (\c -> isDigit c || c == '.') s
+  then Just s
+  else Nothing
+
+parseCountry :: String -> Maybe String
+parseCountry = parseWithPrefix "Country: "
+
+parseCity :: String -> Maybe String
+parseCity = parseWithPrefix "City: "
+
+parseLatitude :: String -> Maybe Double
+parseLatitude "Latitude:" = Just 0
+parseLatitude s = readMaybe =<< parseWithPrefix "Latitude: " s
+
+parseLongitude :: String -> Maybe Double
+parseLongitude "Longitude:" = Just 0
+parseLongitude s = readMaybe =<< parseWithPrefix "Longitude: " s
+
+parseWithPrefix :: String -> String -> Maybe String
+parseWithPrefix prefix s | Just parse <- stripPrefix prefix s = Just parse
+                         | otherwise = Nothing
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,73 @@
+module Main where
+
+import Test.Hspec
+import Data.Time
+import System.Locale (defaultTimeLocale)
+import Dickmine.Parse (parseLogEntry, parseDateString,
+                       parseIP, parseCountry,
+                       parseCity, parseLatitude,
+                       splitDateIPRoute)
+import Dickmine.Types
+
+logEntry :: [String]
+logEntry =
+  [
+    "07/06/2015 08:46:41 am 37.201.171.211 /",
+    "Country: (Unknown Country?) (XX)",
+    "City: (Unknown City?)",
+    "Latitude:",
+    "Longitude:",
+    "IP: 37.201.171.211"
+  ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "split Date + IP + Route Line" $ do
+    it "splits Date from IP from Route" $ do
+      splitDateIPRoute "07/06/2015 08:46:41 am 37.201.171.211 /" `shouldBe` ("07/06/2015 08:46:41 am", "37.201.171.211", "/")
+
+  describe "parse Date" $ do
+    it "fails to parse a malformed date" $ do
+      parseDateString "0213asdjk213" `shouldBe` Nothing
+    it "parses a dateString" $ do
+      parseDateString "07/06/2015 08:46:41 am" `shouldBe` Just (readTime defaultTimeLocale "%m/%d/%Y %I:%M:%S %p" "07/06/2015 08:46:41 AM")
+
+  describe "parse IP" $ do
+    it "fails to parse a malformed IP" $ do
+      parseIP "asdh" `shouldBe` Nothing
+    it "parses a correct IP" $ do
+      parseIP "37.201.171.211" `shouldBe` Just "37.201.171.211"
+
+  describe "parse Country" $ do
+    it "fails to parse a malformed country String" $ do
+      parseCountry "Countasd: (Whatever)" `shouldBe` Nothing
+    it "parses a correct Country String" $ do
+      parseCountry "Country: (Unknown Country?) (XX)" `shouldBe` Just "(Unknown Country?) (XX)"
+
+  describe "parse City" $ do
+    it "fails to parse a malformed city String" $ do
+      parseCity "Countasd: (Whatever)" `shouldBe` Nothing
+    it "parses a correct city String" $ do
+      parseCity "City: (Unknown City?)" `shouldBe` Just "(Unknown City?)"
+
+  describe "parse Latitude" $ do
+    it "failes to parse a malformed Latitude" $ do
+      parseLatitude "Counasdmh:asdkj" `shouldBe` Nothing
+      parseLatitude "Latitude: asd" `shouldBe` Nothing
+
+    it "parses a Latitude" $ do
+      parseLatitude "Latitude:" `shouldBe` Just 0
+      parseLatitude "Latitude: 42.12" `shouldBe` Just 42.12
+
+  describe "parseLogEntry" $ do
+    it "fails to parse on empty Input" $ do
+      parseLogEntry [] `shouldBe` Nothing
+    it "parses a correct Log Entry" $ do
+      parseLogEntry logEntry `shouldBe`
+        Just (Pagehit {
+                  ip = "37.201.171.211",
+                  page = "/",
+                  latitude = 0,
+                  longitude = 0,
+                  timestamp = readTime defaultTimeLocale "%m/%d/%Y %I:%M:%S %p" "07/06/2015 08:46:41 AM"
+                  })
