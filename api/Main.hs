784c94fcd9ed5ed91bb0ceb59ff728d070c80e1c
creek <christoph.hegemann@gmx.net>
2015-07-11 02:51:43 +0200

    set up Query Type

--- api/Main.hs
+++ api/Main.hs
@@ -4,14 +4,49 @@ module Main where
 
 import           Data.List          (nub)
 import           Data.Monoid        (mconcat)
+import           Data.Time
 import           Dickmine.IO
 import qualified Dickmine.Parse     as DP
 import           Dickmine.Query
+import           Dickmine.Types
 import           Pipes
 import qualified Pipes.Prelude      as P
 import           System.Environment
 import           System.IO
 import           Web.Scotty
+
+data Query = Query Filter Aggregation
+
+data Filter = City String
+            | Country String
+            | Url String
+            | Before UTCTime
+            | After UTCTime
+
+data Aggregation = Length
+                 | JSON
+
+data QueryResult a = Raw String
+                   | Encoded a
+                     deriving(Show)
+
+queryPagehits :: Query -> [Pagehit] -> QueryResult [Pagehit]
+queryPagehits (Query f a) ps = aggregate a $ filter' f ps
+  where
+    filter' (City s) = filter ((==) s . city)
+    filter' (Country s) = filter ((==) s . country)
+    filter' (Url s) = filter ((==) s . page)
+    filter' (Before t) = filter ((<) t . timestamp)
+    filter' (After t) = filter ((>=) t . timestamp)
+
+    aggregate Length = Raw . show . length
+    aggregate JSON = Encoded . nub
+
+entries = do
+  hFiles <- mapM (`openFile` ReadMode) ["../data/traffic.log"]
+  P.toListM $ concatLogFiles hFiles >->
+    splitIntoEntries >-> DP.parseLogEntries'
+
 
 main = do
   args <- getArgs
